0f911c41fe9cd057b71e5f750e0e308c2e0189f2
Cindy <cindychynoweth@hotmail.com>
2022-11-11 19:41:15 -0700

    ThoughtSkeleton

--- models/Thought.js
+++ models/Thought.js
@@ -1,34 +1,12 @@
-const { Schema, model } = require('mongoose');
-const reactionSchema = require('./Reaction');
-const dateFormat = require('../utils/dateFormat');
-const thoughtSchema = new Schema(
-  {
-    thoughtText: {
-      type: String,
-      required: 'You need to leave a thought!',
-      minlength: 1,
-      maxlength: 280
-    },
-    createdAt: {
-      type: Date,
-      default: Date.now,
-      get: timestamp => dateFormat(timestamp)
-    },
-    username: {
-      type: String,
-      required: true
-    },
-    reactions: [reactionSchema]
-  },
-  {
-    toJSON: {
-      getters: true
-    },
-    id: false
-  }
-);
-thoughtSchema.virtual('reactionCount').get(function() {
-  return this.reactions.length;
-});
-const Thought = model('Thought', thoughtSchema);
+const { Schema, model, Types } = require("mongoose");
+const dateFormat = require("../utils/dateFormat");
+
+const ReactionSchema = new Schema
+
+
+
+
+
+const Thought = model("Thought", ThoughtSchema);
+
 module.exports = Thought;
