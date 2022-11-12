0d18aecc307b9f71d93fbca713695e3ebc8162ee
Cindy <cindychynoweth@hotmail.com>
2022-11-11 20:55:45 -0700

    Trying to Fix bugs

--- routes/api/index.js
+++ routes/api/index.js
@@ -5,9 +5,4 @@ const thoughtRoutes = require("./thought-routes");
 router.use("/users", userRoutes);
 router.use("/thoughts", thoughtRoutes);
 
-// status error
-// router.use((req, res) => {
-//     res.status(404).send('<h1>😝 404 Error!</h1>');
-//   });
-  
-  module.exports = router;
+module.exports = router;
