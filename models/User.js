1d0de018c1c678a6cdd0c738598577ca6e262bbe
Cindy <cindychynoweth@hotmail.com>
2022-11-11 19:51:33 -0700

    UserSkeleton

--- models/User.js
+++ models/User.js
@@ -1,52 +1,7 @@
-// const { Schema, model } = require("mongoose");
-const Thought = require("./Thought");
+const { Schema, model } = require("mongoose");
 
-const { Schema, model } = require('mongoose');
-const userSchema = new Schema(
-  {
-    username: {
-      type: String,
-      required: true,
-      unique: true,
-      trim: true,
-    },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-      match: [/.+@.+\..+/, 'Must match an email address!'],
-    },
-    thoughts: [
-      {
-        type: Schema.Types.ObjectId,
-        ref: 'Thought',
-      },
-    ],
-    friends: [
-      {
-        type: Schema.Types.ObjectId,
-        ref: 'User',
-      },
-    ],
-  },
-  {
-    toJSON: {
-      virtuals: true,
-    },
-    id: false,
-  }
-);
-userSchema.virtual('friendCount').get(function () {
-  return this.friends.length;
-});
+const UserSchema = new Schema
 
-// BONUS
-userSchema.pre("findOneAndDelete", { document: false, query: true }, async function() {
-    console.log("User pre-delete");
-    const doc = await this.model.findOne(this.getFilter());
-    console.log(doc.username);
-    await Thought.deleteMany({ username: doc.username });
-});
+const User = model("User", UserSchema);
 
-const User = model('User', userSchema);
 module.exports = User;
