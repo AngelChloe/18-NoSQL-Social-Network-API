1e5ad475c2d831131599bfc512807eeca0faa3fb
Cindy <cindychynoweth@hotmail.com>
2022-11-11 20:01:47 -0700

    Fixed const routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,9 +1,12 @@
 const router = require('express').Router();
 // import all API routes from /api/index.js
-const userRoutes = require("./users-routes");
-const userThoughts = require("./thoughts-routes");
+const userRoutes = require("./user-routes");
+const thoughtRoutes = require("./thought-routes");
+
+
 router.use("/users", userRoutes);
 router.use("/thoughts", userThoughts);
+
 // status error
 router.use((req, res) => {
     res.status(404).send('<h1>😝 404 Error!</h1>');
