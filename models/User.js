405536883eade7cbf4c8bc1982b6367ecc5027b9
Cindy <cindychynoweth@hotmail.com>
2022-11-11 19:54:15 -0700

    UserSh=chema

--- models/User.js
+++ models/User.js
@@ -1,7 +1,46 @@
 const { Schema, model } = require("mongoose");
-
-const UserSchema = new Schema
-
 const User = model("User", UserSchema);
+ 
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      trim: true,
+      required: "Username is Required",
+    },
+
+    email: {
+      type: String,
+      unique: true,
+      required: "Username is Required",
+      match: [/.+@.+\..+/],
+    },
+
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+
+
+
 
 module.exports = User;
