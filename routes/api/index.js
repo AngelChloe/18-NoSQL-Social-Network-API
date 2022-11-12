97c0dd03726a9da84d6af197b0e481c598f0d58b
Cindy <cindychynoweth@hotmail.com>
2022-11-11 20:51:12 -0700

    codeChagesHopeullyFixed

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,15 +1,14 @@
 const router = require('express').Router();
-// const index = require("./index")
+const router = require("express").Router();
 const userRoutes = require("./user-routes");
 const thoughtRoutes = require("./thought-routes");
 
-
 router.use("/users", userRoutes);
-router.use("/thoughts", userThoughts);
+router.use("/thoughts", thoughtRoutes);
 
 // status error
-router.use((req, res) => {
-    res.status(404).send('<h1>😝 404 Error!</h1>');
-  });
+// router.use((req, res) => {
+//     res.status(404).send('<h1>😝 404 Error!</h1>');
+//   });
   
   module.exports = router;
