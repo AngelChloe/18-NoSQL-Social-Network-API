ec149b7b460d1695ab5ec4755baba3c56a485a80
Cindy <cindychynoweth@hotmail.com>
2022-11-11 20:06:54 -0700

    Thoughts & ThoughtsID

--- routes/api/thoughts-routes.js
+++ routes/api/thoughts-routes.js
@@ -11,6 +11,14 @@ const {
     removeReaction
 } = require("../../controllers//thought-controller");
 
+// /api/thoughts
+router.route("/").get(getAllThought).post(createThought);
 
+// /api/thoughts/:id
+router
+  .route("/:id")
+  .get(getThoughtById)
+  .put(updateThought)
+  .delete(deleteThought);
 
 module.exports = router;
