a451dd0e13ec2afd6a5665741940c770b1cca58d
Cindy <cindychynoweth@hotmail.com>
2022-11-13 16:16:03 -0700

    Trying to make things work

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
-const express = require("express");
-const db = require("./config/connection");
-const routes = require("./routes");
+const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(routes);
+app.use(express.static('public'));
 
-db.once("open", () => {
-  app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
-  });
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/18-social-network-api', {
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 });
 
+mongoose.set('debug', true);
+
+app.use(require('./routes'));
+
+app.listen(PORT, () => console.log(`Connected on localhost:${PORT}`));
